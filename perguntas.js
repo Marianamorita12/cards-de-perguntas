58a58a8660ab1c4fd3bd7a1d1d715d1926ade168
Marianamorita12 <maricarato.m@gmail.com>
2024-11-22 00:11:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'O que é a segurança cibernética?',
+    'Imagine sua casa. Você tem portas e janelas com fechaduras para proteger seus pertences. No mundo digital, a segurança cibernética é como essas fechaduras. Ela protege seus dados, computadores e redes de intrusos que podem querer roubar suas informações, causar danos ou interromper seus serviços.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é a inteligência artificial?',
+    'A IA é como criar um cérebro para um computador. Ela permite que máquinas aprendam, pensem e tomem decisões por conta própria. A IA é usada em diversas áreas, desde carros autônomos até assistentes virtuais como a Siri ou o Google Assistente.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é um algoritmo?',
+    'Um algoritmo é como uma receita de bolo. É um conjunto de instruções passo a passo que um computador segue para realizar uma tarefa. Os algoritmos são a base da programação e são usados em tudo, desde jogos de computador até softwares de contabilidade.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'o que é o 5G?',
+    'O 5G é a quinta geração de redes móveis. É a tecnologia que está substituindo o 4G e promete velocidades de internet muito mais rápidas, menor latência (tempo de resposta) e a capacidade de conectar muitos mais dispositivos à rede ao mesmo tempo.'
+)
